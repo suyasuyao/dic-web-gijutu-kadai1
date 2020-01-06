a008c2f41e96fac73fd0eea34ee9cc6b7b799844
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-07 04:07:33 +0900

    二つの実装
    ボタンを押すと、譲渡先が自家消費ではないもののgoyaの情報を載せたページが表示される機能を実装すること
    ボタンを押すと、品質が悪いもの（false）のgoyaの情報を載せたページが表示される機能を実装すること

--- test.rb
+++ test.rb
@@ -11,9 +11,9 @@ server = WEBrick::HTTPServer.new({
 
 # この一行を追記
 server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'show.html.erb')
+server.mount('/othergoya.cgi', WEBrick::HTTPServlet::CGIHandler, 'othergoya.rb')
+server.mount('/falsegoya.cgi', WEBrick::HTTPServlet::CGIHandler, 'falsegoya.rb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
-# server.mount('/othergoya.cgi', WEBrick::HTTPServlet::CGIHandler, 'othergoya.rb')
-# server.mount('/falsegoya.cgi', WEBrick::HTTPServlet::CGIHandler, 'falsegoya.rb')
 server.start
